fe4d8ce855e8dd39ab850dfd0a664a4868e5be8e
namratachavan24 <109748579+namratachavan24@users.noreply.github.com>
2022-08-22 23:20:14 +0530

    UC-2 Find Large Paragraph Phrase

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace HashTable
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            FrequencyOfWords<string, string> hash = new FrequencyOfWords<string, string>(5);
+
+            hash.GetFreq();
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
